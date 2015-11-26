8ded7700923f29063c9ede89dfdca8711b74f0f1
Vadym Borodin <borodean@gmail.com>
2015-11-26 23:32:53 +0200

    Use real file fixtures instead of a mock

--- test/resolve-path.js
+++ test/resolve-path.js
@@ -1,34 +1,10 @@
-var mock = require('mock-fs');
 var path = require('path');
 var resolvePath = require('../lib/resolve-path');
 var test = require('ava');
 
-test.before(function (t) {
-  mock({
-    'assets': {
-      'duplicate-1.jpg': new Buffer(0),
-      'fonts': {
-        'duplicate-2.txt': new Buffer(0),
-        'pt-sans.woff': new Buffer(0)
-      },
-      'images': {
-        'duplicate-1.jpg': new Buffer(0),
-        'duplicate-2.txt': new Buffer(0),
-        'picture.png': new Buffer(0)
-      }
-    }
-  });
-  t.end();
-});
-
-test.after(function (t) {
-  mock.restore();
-  t.end();
-});
-
 test('resolves from the current working directory', function (t) {
-  return resolvePath('assets/images/picture.png').then(function (resolvedPath) {
-    t.is(resolvedPath, path.resolve('assets/images/picture.png'));
+  return resolvePath('fixtures/images/picture.png').then(function (resolvedPath) {
+    t.is(resolvedPath, path.resolve('fixtures/images/picture.png'));
   }, t.fail);
 });
 
@@ -41,52 +17,52 @@ test('rejects when resolving non-exisiting file', function (t) {
 
 test('resolves from the base path', function (t) {
   return resolvePath('picture.png', {
-    basePath: 'assets/images'
+    basePath: 'fixtures/images'
   }).then(function (resolvedPath) {
-    t.is(resolvedPath, path.resolve('assets/images/picture.png'));
+    t.is(resolvedPath, path.resolve('fixtures/images/picture.png'));
   }, t.fail);
 });
 
 test('resolves from the load path', function (t) {
-  return resolvePath('pt-sans.woff', {
-    loadPaths: ['assets/fonts', 'assets/images']
+  return resolvePath('empty-sans.woff', {
+    loadPaths: ['fixtures/fonts', 'fixtures/images']
   }).then(function (resolvedPath) {
-    t.is(resolvedPath, path.resolve('assets/fonts/pt-sans.woff'));
+    t.is(resolvedPath, path.resolve('fixtures/fonts/empty-sans.woff'));
   }, t.fail);
 });
 
 test('resolves from the base path and load path combo', function (t) {
-  return resolvePath('pt-sans.woff', {
-    basePath: 'assets',
+  return resolvePath('empty-sans.woff', {
+    basePath: 'fixtures',
     loadPaths: ['fonts', 'images']
   }).then(function (resolvedPath) {
-    t.is(resolvedPath, path.resolve('assets/fonts/pt-sans.woff'));
+    t.is(resolvedPath, path.resolve('fixtures/fonts/empty-sans.woff'));
   }, t.fail);
 });
 
 test('prioritizes the base path over the load path', function (t) {
   return resolvePath('duplicate-1.jpg', {
-    basePath: 'assets',
+    basePath: 'fixtures',
     loadPaths: ['fonts', 'images']
   }).then(function (resolvedPath) {
-    t.is(resolvedPath, path.resolve('assets/duplicate-1.jpg'));
+    t.is(resolvedPath, path.resolve('fixtures/duplicate-1.jpg'));
   }, t.fail);
 });
 
 test('prioritizes the load paths by their order', function (t) {
   return resolvePath('duplicate-2.txt', {
-    basePath: 'assets',
+    basePath: 'fixtures',
     loadPaths: ['fonts', 'images']
   }).then(function (resolvedPath) {
-    t.is(resolvedPath, path.resolve('assets/fonts/duplicate-2.txt'));
+    t.is(resolvedPath, path.resolve('fixtures/fonts/duplicate-2.txt'));
   }, t.fail);
 });
 
 test('accepts node-style callback', function (t) {
   t.plan(5);
 
-  resolvePath('assets/images/picture.png', function (err, resolvedPath) {
-    t.is(resolvedPath, path.resolve('assets/images/picture.png'));
+  resolvePath('fixtures/images/picture.png', function (err, resolvedPath) {
+    t.is(resolvedPath, path.resolve('fixtures/images/picture.png'));
   });
 
   resolvePath('not-found.gif', function (err, resolvedPath) {
@@ -96,8 +72,8 @@ test('accepts node-style callback', function (t) {
   });
 
   resolvePath('picture.png', {
-    basePath: 'assets/images'
+    basePath: 'fixtures/images'
   }, function (err, resolvedPath) {
-    t.is(resolvedPath, path.resolve('assets/images/picture.png'));
+    t.is(resolvedPath, path.resolve('fixtures/images/picture.png'));
   });
 });
--- test/resolve-url.js
+++ test/resolve-url.js
@@ -1,33 +1,9 @@
-var mock = require('mock-fs');
 var resolveUrl = require('../lib/resolve-url');
 var test = require('ava');
 
-test.before(function (t) {
-  mock({
-    'assets': {
-      'duplicate-1.jpg': new Buffer(0),
-      'fonts': {
-        'duplicate-2.txt': new Buffer(0),
-        'pt-sans.woff': new Buffer(0)
-      },
-      'images': {
-        'duplicate-1.jpg': new Buffer(0),
-        'duplicate-2.txt': new Buffer(0),
-        'picture.png': new Buffer(0)
-      }
-    }
-  });
-  t.end();
-});
-
-test.after(function (t) {
-  mock.restore();
-  t.end();
-});
-
 test('resolves from the current working directory', function (t) {
-  return resolveUrl('assets/images/picture.png').then(function (resolvedUrl) {
-    t.is(resolvedUrl, '/assets/images/picture.png');
+  return resolveUrl('fixtures/images/picture.png').then(function (resolvedUrl) {
+    t.is(resolvedUrl, '/fixtures/images/picture.png');
   }, t.fail);
 });
 
@@ -40,48 +16,48 @@ test('rejects when resolving non-exisiting file', function (t) {
 
 test('resolves from the base path', function (t) {
   return resolveUrl('picture.png', {
-    basePath: 'assets/images'
+    basePath: 'fixtures/images'
   }).then(function (resolvedUrl) {
     t.is(resolvedUrl, ('/picture.png'));
   }, t.fail);
 });
 
 test('resolves from the load path', function (t) {
-  return resolveUrl('pt-sans.woff', {
-    loadPaths: ['assets/fonts', 'assets/images']
+  return resolveUrl('empty-sans.woff', {
+    loadPaths: ['fixtures/fonts', 'fixtures/images']
   }).then(function (resolvedUrl) {
-    t.is(resolvedUrl, '/assets/fonts/pt-sans.woff');
+    t.is(resolvedUrl, '/fixtures/fonts/empty-sans.woff');
   }, t.fail);
 });
 
 test('resolves from the base path and load path combo', function (t) {
-  return resolveUrl('pt-sans.woff', {
-    basePath: 'assets',
+  return resolveUrl('empty-sans.woff', {
+    basePath: 'fixtures',
     loadPaths: ['fonts', 'images']
   }).then(function (resolvedUrl) {
-    t.is(resolvedUrl, '/fonts/pt-sans.woff');
+    t.is(resolvedUrl, '/fonts/empty-sans.woff');
   }, t.fail);
 });
 
 test('resolves from the base URL with a trailing slash', function (t) {
-  return resolveUrl('assets/images/picture.png', {
+  return resolveUrl('fixtures/images/picture.png', {
     baseUrl: 'http://example.com/wp-content/themes/'
   }).then(function (resolvedUrl) {
-    t.is(resolvedUrl, ('http://example.com/wp-content/themes/assets/images/picture.png'));
+    t.is(resolvedUrl, ('http://example.com/wp-content/themes/fixtures/images/picture.png'));
   }, t.fail);
 });
 
 test('resolves from the base URL without a trailing slash', function (t) {
-  return resolveUrl('assets/images/picture.png', {
+  return resolveUrl('fixtures/images/picture.png', {
     baseUrl: 'http://example.com/wp-content/themes'
   }).then(function (resolvedUrl) {
-    t.is(resolvedUrl, ('http://example.com/wp-content/themes/assets/images/picture.png'));
+    t.is(resolvedUrl, ('http://example.com/wp-content/themes/fixtures/images/picture.png'));
   }, t.fail);
 });
 
 test('resolves from the base URL and base path combo', function (t) {
   return resolveUrl('images/picture.png', {
-    basePath: 'assets',
+    basePath: 'fixtures',
     baseUrl: 'http://example.com/wp-content/themes/'
   }).then(function (resolvedUrl) {
     t.is(resolvedUrl, ('http://example.com/wp-content/themes/images/picture.png'));
@@ -89,18 +65,18 @@ test('resolves from the base URL and base path combo', function (t) {
 });
 
 test('resolves from the base URL, base path and load path combo', function (t) {
-  return resolveUrl('pt-sans.woff', {
-    basePath: 'assets',
+  return resolveUrl('empty-sans.woff', {
+    basePath: 'fixtures',
     baseUrl: 'http://example.com/wp-content/themes/',
     loadPaths: ['fonts', 'images']
   }).then(function (resolvedUrl) {
-    t.is(resolvedUrl, ('http://example.com/wp-content/themes/fonts/pt-sans.woff'));
+    t.is(resolvedUrl, ('http://example.com/wp-content/themes/fonts/empty-sans.woff'));
   }, t.fail);
 });
 
 test('resolves to a relative URL', function (t) {
-  return resolveUrl('assets/images/picture.png', {
-    relativeTo: 'assets/fonts'
+  return resolveUrl('fixtures/images/picture.png', {
+    relativeTo: 'fixtures/fonts'
   }).then(function (resolvedUrl) {
     t.is(resolvedUrl, '../images/picture.png');
   }, t.fail);
@@ -108,7 +84,7 @@ test('resolves to a relative URL', function (t) {
 
 test('resolves to a relative URL from the base path', function (t) {
   return resolveUrl('images/picture.png', {
-    basePath: 'assets',
+    basePath: 'fixtures',
     relativeTo: 'fonts'
   }).then(function (resolvedUrl) {
     t.is(resolvedUrl, '../images/picture.png');
@@ -117,15 +93,15 @@ test('resolves to a relative URL from the base path', function (t) {
 
 test('resolves keeping query parameters and hash', function (t) {
   return resolveUrl('images/picture.png?foo=bar&baz#hash', {
-    basePath: 'assets'
+    basePath: 'fixtures'
   }).then(function (resolvedUrl) {
     t.is(resolvedUrl, '/images/picture.png?foo=bar&baz#hash');
   }, t.fail);
 });
 
 test('resolves to a relative URL keeping query parameters and hash', function (t) {
-  return resolveUrl('assets/images/picture.png?foo=bar&baz#hash', {
-    relativeTo: 'assets/fonts'
+  return resolveUrl('fixtures/images/picture.png?foo=bar&baz#hash', {
+    relativeTo: 'fixtures/fonts'
   }).then(function (resolvedUrl) {
     t.is(resolvedUrl, '../images/picture.png?foo=bar&baz#hash');
   }, t.fail);
@@ -134,8 +110,8 @@ test('resolves to a relative URL keeping query parameters and hash', function (t
 test('accepts node-style callback', function (t) {
   t.plan(5);
 
-  resolveUrl('assets/images/picture.png', function (err, resolvedUrl) {
-    t.is(resolvedUrl, '/assets/images/picture.png');
+  resolveUrl('fixtures/images/picture.png', function (err, resolvedUrl) {
+    t.is(resolvedUrl, '/fixtures/images/picture.png');
   });
 
   resolveUrl('not-found.gif', function (err, resolvedUrl) {
@@ -145,7 +121,7 @@ test('accepts node-style callback', function (t) {
   });
 
   resolveUrl('picture.png', {
-    basePath: 'assets/images'
+    basePath: 'fixtures/images'
   }, function (err, resolvedUrl) {
     t.is(resolvedUrl, '/picture.png');
   });
