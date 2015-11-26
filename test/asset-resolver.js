1a19a1c35ff675a18be2ba9c6f56236dcf81fb82
Vadym Borodin <borodean@gmail.com>
2015-11-26 13:38:36 +0200

    Implement AssetResolver.options

--- test/asset-resolver.js
+++ test/asset-resolver.js
@@ -2,7 +2,16 @@ var AssetResolver = require('../lib/asset-resolver');
 var test = require('ava');
 
 test('AssetResolver', function (t) {
-  t.same(typeof AssetResolver, 'function');
-  t.ok(AssetResolver() instanceof AssetResolver);
+  t.same(typeof AssetResolver, 'function', 'is a function');
+  t.ok(AssetResolver() instanceof AssetResolver, 'instantiable without new');
+  t.ok(Object.isFrozen(AssetResolver()), 'is frozen');
+  t.end();
+});
+
+test('AssetResolver.options', function (t) {
+  var options = { basePath: 'source' };
+  t.same(AssetResolver().options, {}, 'defaults to an empty object');
+  t.same(AssetResolver(options).options.basePath, 'source', 'is initiable');
+  t.not(AssetResolver(options).options, options, 'breaks the reference');
   t.end();
 });
