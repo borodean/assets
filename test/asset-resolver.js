1a4e4d02cd238ad08d4ec2dede0b2fb294aee162
Vadym Borodin <borodean@gmail.com>
2015-11-27 16:38:21 +0200

    Tidy and test proxying to AssetResolver.prototype

--- test/asset-resolver.js
+++ test/asset-resolver.js
@@ -1,5 +1,22 @@
 var AssetResolver = require('..');
+var sinon = require('sinon');
 var test = require('ava');
+
+test.before(function (t) {
+  sinon.stub(AssetResolver, 'measure');
+  sinon.stub(AssetResolver, 'resolveDataUrl');
+  sinon.stub(AssetResolver, 'resolvePath');
+  sinon.stub(AssetResolver, 'resolveUrl');
+  t.end();
+});
+
+test.after(function (t) {
+  AssetResolver.measure.restore();
+  AssetResolver.resolveDataUrl.restore();
+  AssetResolver.resolvePath.restore();
+  AssetResolver.resolveUrl.restore();
+  t.end();
+});
 
 test('AssetResolver', function (t) {
   t.same(typeof AssetResolver, 'function', 'is a function');
@@ -13,5 +30,29 @@ test('AssetResolver.options', function (t) {
   t.same(AssetResolver().options, {}, 'defaults to an empty object');
   t.same(AssetResolver(options).options.basePath, 'source', 'is initiable');
   t.not(AssetResolver(options).options, options, 'breaks the reference');
+  t.end();
+});
+
+test('AssetResolver.measure()', function (t) {
+  AssetResolver().measure();
+  t.ok(AssetResolver.measure.called);
+  t.end();
+});
+
+test('AssetResolver.resolveDataUrl()', function (t) {
+  AssetResolver().resolveDataUrl();
+  t.ok(AssetResolver.resolveDataUrl.called);
+  t.end();
+});
+
+test('AssetResolver.resolvePath()', function (t) {
+  AssetResolver().resolvePath();
+  t.ok(AssetResolver.resolvePath.called);
+  t.end();
+});
+
+test('AssetResolver.resolveUrl()', function (t) {
+  AssetResolver().resolveUrl();
+  t.ok(AssetResolver.resolveUrl.called);
   t.end();
 });
