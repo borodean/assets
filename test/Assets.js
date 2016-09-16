9075d632f053cee5808a3fbfa69fae269970f411
Vadym Borodin <borodean@gmail.com>
2016-09-16 03:20:15 +0300

    Make ESLint happy

--- test/Assets.js
+++ test/Assets.js
@@ -18,33 +18,38 @@ test.after(() => {
 
 test('constructor', (t) => {
   t.same(typeof Assets, 'function', 'is a function');
+  // eslint-disable-next-line new-cap
   t.ok(Assets() instanceof Assets, 'instantiable without new');
-  t.ok(Object.isFrozen(Assets()), 'is frozen');
+  t.ok(Object.isFrozen(new Assets()), 'is frozen');
 });
 
 test('.options', (t) => {
   const options = { basePath: 'source' };
-  t.same(Assets().options, {}, 'defaults to an empty object');
-  t.same(Assets(options).options.basePath, 'source', 'is initiable');
-  t.not(Assets(options).options, options, 'breaks the reference');
+  t.same(new Assets().options, {}, 'defaults to an empty object');
+  t.same(new Assets(options).options.basePath, 'source', 'is initiable');
+  t.not(new Assets(options).options, options, 'breaks the reference');
 });
 
 test('.data()', (t) => {
-  Assets().data();
+  const instance = new Assets();
+  instance.data();
   t.ok(Assets.data.called);
 });
 
 test('.path()', (t) => {
-  Assets().path();
+  const instance = new Assets();
+  instance.path();
   t.ok(Assets.path.called);
 });
 
 test('.size()', (t) => {
-  Assets().size();
+  const instance = new Assets();
+  instance.size();
   t.ok(Assets.size.called);
 });
 
 test('.url()', (t) => {
-  Assets().url();
+  const instance = new Assets();
+  instance.url();
   t.ok(Assets.url.called);
 });
